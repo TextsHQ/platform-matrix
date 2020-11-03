7dcdc7e01039638eeeeb0e9cdb9a63d39395c88c
Ping Chen <remotenonsense@gmail.com>
2020-11-03 20:59:21 +0900

    Support action types in mapMessage

--- src/mappers.ts
+++ src/mappers.ts
@@ -40,22 +40,80 @@ export function mapRoom(userID, room): Thread {
 }
 
 export function mapMessage(userID, event): Message {
-  let body
-  if (event.getType() === 'm.room.message') {
-    body = event.getContent().body
-  } else {
-    return null
+  let text
+  let action = null
+  const senderID = event.getSender()
+
+  switch (event.getType()) {
+    case 'm.room.encryption': {
+      action = {
+        type: MessageActionType.THREAD_TITLE_UPDATED,
+        title: 'Encrypted',
+        actorParticipantID: senderID,
+      }
+      text = 'Encryption enabled'
+      break
+    }
+    case 'm.room.member': {
+      let membership = event.getContent().membership
+      let type
+      if (membership == 'join') {
+        type = MessageActionType.THREAD_PARTICIPANTS_ADDED
+        text = `${senderID} joined the room`
+      } else if (membership == 'leave') {
+        type = MessageActionType.THREAD_PARTICIPANTS_REMOVED
+        text = `${senderID} left the room`
+      } else {
+        console.log('-- m.room.member', event)
+        return null
+      }
+      action = {
+        type,
+        title: event.getContent().membership,
+        actorParticipantID: senderID,
+      }
+      break
+    }
+    case 'm.room.message': {
+      text = event.getContent().body
+      break
+    }
+    case 'm.room.name': {
+      const prevContent = event.getPrevContent()
+      let type
+      if (prevContent.name) {
+        type = MessageActionType.THREAD_TITLE_UPDATED
+        text = `${senderID} changed the room name from ${prevContent.name} to ${
+          event.getContent().name
+        }`
+      } else {
+        type = MessageActionType.GROUP_THREAD_CREATED
+        text = `${senderID} created and configured the room`
+      }
+      // console.log('m.room.name', event.getPrevContent())
+      action = {
+        type,
+        title: event.getContent().name,
+        actorParticipantID: senderID,
+      }
+      break
+    }
+    default: {
+      console.log('-- mapMessage', event)
+      return null
+    }
   }
 
-  const senderID = event.getSender()
   return {
     _original: [],
     id: event.getId(),
-    timestamp: new Date(event.getTs()),
+    timestamp: event.getDate(),
     senderID,
-    text: body,
+    text,
     isSender: userID == senderID,
     attachments: [],
+    isAction: !!action,
+    action,
     reactions: [],
   }
 }
