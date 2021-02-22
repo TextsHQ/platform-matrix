82134ffd2528e051fb7fd912ca9a97a9c7fbf77f
Ping Chen <remotenonsense@gmail.com>
2021-02-22 10:26:37 +0900

    Use commonmark to parse message text

---
+++ src/tests/text-attributes.test.ts
@@ -0,0 +1,56 @@
+import { mapTextAttributes } from '../text-attributes'
+
+const cases = [
+  {
+    text: '\n*italic*\n **bold**\n\n plain',
+    result: {
+      text: 'italic\nbold\n\nplain',
+      textAttributes: {
+        entities: [
+          {
+            from: 0,
+            to: 6,
+            italic: true,
+          },
+          {
+            from: 7,
+            to: 11,
+            bold: true,
+          },
+        ],
+      },
+    },
+  },
+  {
+    text:
+      '**[[tulir/mautrix-wsproxy](https://github.com/tulir/mautrix-wsproxy)]** [Yumekui](https://github.com/Yumekui) opened pull request #1',
+    result: {
+      text: '[tulir/mautrix-wsproxy] Yumekui opened pull request #1',
+      textAttributes: {
+        entities: [
+          {
+            from: 1,
+            to: 22,
+            link: 'https://github.com/tulir/mautrix-wsproxy',
+          },
+          {
+            from: 0,
+            to: 23,
+            bold: true,
+          },
+          {
+            from: 24,
+            to: 31,
+            link: 'https://github.com/Yumekui',
+          },
+        ],
+      },
+    },
+  },
+]
+
+test('text attributes', () => {
+  for (const c of cases) {
+    expect(mapTextAttributes(c.text)).toEqual(c.result)
+  }
+})
---
+++ src/text-attributes.ts
@@ -0,0 +1,63 @@
+import { TextEntity } from '@textshq/platform-sdk'
+import { Parser } from 'commonmark'
+
+const reader = new Parser({ safe: true })
+
+// All types: text, softbreak, linebreak, emph, strong, html_inline, link,
+// image, code, document, paragraph, block_quote, item, list, heading,
+// code_block, html_block, thematic_break
+const TOKEN_TYPES = ['emph', 'strong', 'link', 'code']
+
+export function mapTextAttributes(src: string) {
+  if (!src) return
+  const parsed = reader.parse(src)
+  const walker = parsed.walker()
+
+  let output = ''
+  const entities: TextEntity[] = []
+
+  let tokenStack = []
+  let event
+  while ((event = walker.next())) {
+    const { node } = event
+    if (event.entering) {
+      if (TOKEN_TYPES.includes(node.type)) {
+        tokenStack.push({
+          type: node.type,
+          from: output.length,
+        })
+      } else if (['softbreak', 'linkebreak'].includes(node.type)) {
+        output += '\n'
+      } else if (node.type === 'text') {
+        output += node.literal
+      }
+    } else {
+      if (node.type === 'paragraph') {
+        output += '\n\n'
+        continue
+      }
+      const lastToken = tokenStack.pop()
+      if (!lastToken) continue
+      const entity: TextEntity = {
+        from: lastToken.from,
+        to: output.length,
+      }
+      switch (lastToken.type) {
+        case 'emph':
+          entity.italic = true
+          break
+        case 'strong':
+          entity.bold = true
+          break
+        case 'link':
+          entity.link = node.destination
+          break
+      }
+      entities.push(entity)
+    }
+  }
+  return {
+    text: output.trim(),
+    textAttributes: entities.length ? { entities } : undefined,
+  }
+}
