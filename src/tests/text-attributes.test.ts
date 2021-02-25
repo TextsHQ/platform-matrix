3d38b52858bdeae51fac7b576f7449a0eebd3ec2
Ping Chen <remotenonsense@gmail.com>
2021-02-25 22:51:05 +0900

    Parse text to entities with commonmark (#11)

---
+++ src/tests/text-attributes.test.ts
@@ -0,0 +1,84 @@
+import { mapTextAttributes } from '../text-attributes'
+
+const cases = [
+  {
+    text: '\n*italic*\n **bold**\n\n plain <del>st</del> `code`.',
+    result: {
+      text: 'italic\nbold\n\nplain st code.',
+      textAttributes: {
+        entities: [
+          {
+            from: 0,
+            to: 6,
+            italic: true,
+          },
+          {
+            from: 7,
+            to: 11,
+            bold: true,
+          },
+          {
+            from: 19,
+            to: 21,
+            strikethrough: true,
+          },
+          {
+            from: 22,
+            to: 26,
+            code: true,
+          },
+        ],
+      },
+    },
+  },
+  {
+    text:
+      '**[[tulir/mautrix-wsproxy](https://github.com/tulir/mautrix-wsproxy)]** [Yumekui](https://github.com/Yumekui) opened pull request #1',
+    result: {
+      text: '[tulir/mautrix-wsproxy] Yumekui opened pull request #1',
+      textAttributes: {
+        entities: [
+          {
+            from: 1,
+            to: 22,
+            link: 'https://github.com/tulir/mautrix-wsproxy',
+          },
+          {
+            from: 0,
+            to: 23,
+            bold: true,
+          },
+          {
+            from: 24,
+            to: 31,
+            link: 'https://github.com/Yumekui',
+          },
+        ],
+      },
+    },
+  },
+  {
+    text:
+    '```js\nconsole.log("matrix")\n```',
+    result: {
+      text: 'console.log("matrix")',
+      textAttributes: {
+        entities: [
+          {
+            from: 0,
+            to: 22,
+            pre: true,
+            code: true,
+            codeLanguage: 'js'
+          },
+        ],
+      },
+    },
+  },
+]
+
+test('text attributes', () => {
+  for (const c of cases) {
+    expect(mapTextAttributes(c.text)).toEqual(c.result)
+  }
+})
---
+++ src/text-attributes.ts
@@ -0,0 +1,100 @@
+import { TextEntity } from '@textshq/platform-sdk'
+import { Parser } from 'commonmark'
+
+const reader = new Parser({ safe: true })
+
+// All types: text, softbreak, linebreak, emph, strong, html_inline, link,
+// image, code, document, paragraph, block_quote, item, list, heading,
+// code_block, html_block, thematic_break
+const TOKEN_TYPES = ['emph', 'strong', 'link']
+
+export function mapTextAttributes(src: string) {
+  if (!src) return
+  const parsed = reader.parse(src)
+  const walker = parsed.walker()
+
+  let output = ''
+  const entities: TextEntity[] = []
+
+  let tokenStack = []
+  let event
+  while ((event = walker.next())) {
+    const { node } = event
+    // Some node types have both entering (true) and entering (false), but
+    // others only have entering(true).
+    if (event.entering) {
+      if (TOKEN_TYPES.includes(node.type)) {
+        tokenStack.push({
+          type: node.type,
+          from: output.length,
+        })
+      } else if (node.type === 'html_inline') {
+        if (node.literal.toLowerCase() === '<del>') {
+          tokenStack.push({
+            type: 'del',
+            from: output.length,
+          })
+        } else if (node.literal.toLowerCase() === '</del>') {
+          const lastToken = tokenStack.pop()
+          if (!lastToken) continue
+          if (lastToken.type !== 'del') {
+            tokenStack.push(lastToken)
+            continue
+          }
+          entities.push({
+            from: lastToken.from,
+            to: output.length,
+            strikethrough: true,
+          })
+        }
+      } else if (node.type === 'code') {
+        entities.push({
+          from: output.length,
+          to: output.length + node.literal.length,
+          code: true,
+        })
+        output += node.literal
+      } else if (node.type === 'code_block') {
+        entities.push({
+          from: output.length,
+          to: output.length + node.literal.length,
+          pre: true,
+          code: true,
+          codeLanguage: node.info,
+        })
+        output += node.literal
+      } else if (['softbreak', 'linkebreak'].includes(node.type)) {
+        output += '\n'
+      } else if (node.type === 'text') {
+        output += node.literal
+      }
+    } else {
+      if (node.type === 'paragraph') {
+        output += '\n\n'
+        continue
+      }
+      const lastToken = tokenStack.pop()
+      if (!lastToken) continue
+      const entity: TextEntity = {
+        from: lastToken.from,
+        to: output.length,
+      }
+      switch (lastToken.type) {
+        case 'emph':
+          entity.italic = true
+          break
+        case 'strong':
+          entity.bold = true
+          break
+        case 'link':
+          entity.link = node.destination
+          break
+      }
+      entities.push(entity)
+    }
+  }
+  return {
+    text: output.trim(),
+    textAttributes: entities.length ? { entities } : undefined,
+  }
+}
